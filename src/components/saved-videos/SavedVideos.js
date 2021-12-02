b47df082b9d132d35d186fb11271930b69cdedca
Ankit Negi <ankitn1311@gmail.com>
2021-12-03 00:51:41 +0530

    Saved Videos UI completed

--- src/components/saved-videos/SavedVideos.js
+++ src/components/saved-videos/SavedVideos.js
@@ -1,8 +1,24 @@
 import React from "react";
+import girlImg from "../../assets/images/girl.png";
 import "./SavedVideos.scss";
 
 const SavedVideos = () => {
-  return <div className="SavedVideos">This is saved videos component</div>;
+  const cards = [0];
+  return (
+    <div className="SavedVideos">
+      {cards.map(() => (
+        <div className="SavedVideos__card">
+          <img src={girlImg} className="SavedVideos__card--image" />
+          <div className="SavedVideos__card--title">Saying Hi to users!</div>
+          <div className="SavedVideos__card--badges">
+            <div className="SavedVideos__card--badge">Email</div>
+            <div className="SavedVideos__card--badge">Marketing</div>
+            <div className="SavedVideos__card--badge">Greeting</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default SavedVideos;
